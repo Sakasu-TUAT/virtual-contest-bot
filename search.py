62c355c870f52d5c2faa8614321651a47ae2dcb0
Sakasu02 <s214229y@st.go.tuat.ac.jp>
2023-07-20 03:32:57 +0900

    feat: get elems from ばちゃ

--- search.py
+++ search.py
@@ -5,6 +5,9 @@ from selenium.webdriver.chrome.options import Options
 import time 
 from bs4 import BeautifulSoup
 from datetime import date
+import re
+from test import tweet
+
 today = date.today()
 print("Today's date:", today)
 
@@ -17,46 +20,71 @@ options = Options()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_argument("--headless")
 browser = webdriver.Chrome(options=options)
-# contest_url = 'https://www.yahoo.co.jp/'
-contest_url = 'https://kenkoooo.com/atcoder/#/contest/recent/'
-browser.get(contest_url)
-elements = browser.find_elements(By.TAG_NAME, "h2")
-elements = toStr(elements)
-print(elements)
-elements = browser.find_elements(By.CSS_SELECTOR, "*")
+virtual_contests_url = 'https://kenkoooo.com/atcoder/#/contest/recent/'
+browser.get(virtual_contests_url)
+# elements = browser.find_elements(By.TAG_NAME, "h2")
+# elements = toStr(elements)
+# print(elements)
+# elements = browser.find_elements(By.CSS_SELECTOR, "#root")
+elements = browser.find_elements(By.TAG_NAME, "*")
+# elements = browser.find_elements(By.CLASS_NAME, "*")
+# a = elements[0].get_attribute("innerHTML")
 # 要素の数を表示
-print(len(elements))
-for element in elements:
+# print(a)
+# print(len(elements))
+
+
+
+
+# pattern = r"[Tt][Uu][Aa][Tt]"
+pattern = r"[A][B][C]"
+tweet_urls = []
+
+notable_contest = set()
+
+st = set()
+for element in elements:  
     if element is None: continue
-    innerHTML = element.get_attribute("innerHTML")
-    # BeautifulSoupでinnerHTMLからaタグの要素をすべて取得
+    try:
+        innerHTML = element.get_attribute("outerHTML")
+    except:
+        print("error")
+
+    if st.__contains__(innerHTML) : continue
     if innerHTML is None: continue
+    st.add(innerHTML)
     soup = BeautifulSoup(innerHTML, "html.parser")
-    links = soup.find_all("a")
-    tds = soup.find_all("td")
-    if len(tds) is not 0:
-        print(tds)
-        # print("\n")
-        url = ""
-        contest_date = ""
-        result = False
-        for i in range(len(tds)):
-            if tds[i] is not None:
-                if i==0 :
-                    a = tds[i].find("a")
-                    if a:
-                        result |= True
-                        url = a.get("href")
-                        # print(a)
-                        print("☆ URL: ", url, "☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
-                        # print("☆ tds[0]: ", tds[i], "☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
-                if i==3 :
-                    result |= True
-                    contest_date = tds[i].getText()
-        if result :
-            print("TTTTTTTTTTTT: ", "https://kenkoooo.com/atcoder/"+url, contest_date)
+    tr = soup.find_all("tr")
+
+    for t in tr:
+        # print("%%%%%% ", t, " &&&&&", t.text)
+        a = t.find("a")
+        if a:
+            url = a.get("href")
+            # print("☆ URL: ", url, "☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
+            title = a.getText().replace("Public ", "")
+        if(len(t.find_all("div")) < 3) : continue    
+        [mode, start, end, *remains] = t.find_all("div")
+
+        text = t.getText()
+        # print(text)
+
+        if re.search(pattern, text):
+            # print(mode.text, start.text, end.text, url)
+            # print(t.text)
+            contest_url = 'https://kenkoooo.com/atcoder/'+url
+            # print(contest_url, start)
+            notable_contest.add((title, start.text, end.text, contest_url))
+
+try :
+    print(notable_contest)
+    [title, start, end, contest_url] = notable_contest.pop()
     
-        print("#######################################################\n")
+    tweet(f"test: コンテストのお知らせ -> {title} {contest_url}\
+    ※これはAPIを使った自動ツイートです\
+    #AtCoderProblems")
+except :
+    print("error")
 
 
 browser.quit()
