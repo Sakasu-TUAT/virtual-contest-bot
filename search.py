f439d84a567cd9eeb80ac7d9e70fa3e06c621170
Sakasu02 <s214229y@st.go.tuat.ac.jp>
2023-07-20 03:35:33 +0900

    clean: rm unnecessary codes

--- search.py
+++ search.py
@@ -11,35 +11,16 @@ from test import tweet
 today = date.today()
 print("Today's date:", today)
 
-
-def toStr(elements):
-    elements = [element.text for element in elements]
-    return elements
-
 options = Options()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_argument("--headless")
 browser = webdriver.Chrome(options=options)
 virtual_contests_url = 'https://kenkoooo.com/atcoder/#/contest/recent/'
 browser.get(virtual_contests_url)
-# elements = browser.find_elements(By.TAG_NAME, "h2")
-# elements = toStr(elements)
-# print(elements)
-# elements = browser.find_elements(By.CSS_SELECTOR, "#root")
 elements = browser.find_elements(By.TAG_NAME, "*")
-# elements = browser.find_elements(By.CLASS_NAME, "*")
-# a = elements[0].get_attribute("innerHTML")
-# 要素の数を表示
-# print(a)
-# print(len(elements))
-
-
-
 
 # pattern = r"[Tt][Uu][Aa][Tt]"
 pattern = r"[A][B][C]"
-tweet_urls = []
-
 notable_contest = set()
 
 st = set()
@@ -57,34 +38,27 @@ for element in elements:
     tr = soup.find_all("tr")
 
     for t in tr:
-        # print("%%%%%% ", t, " &&&&&", t.text)
         a = t.find("a")
         if a:
             url = a.get("href")
-            # print("☆ URL: ", url, "☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
             title = a.getText().replace("Public ", "")
         if(len(t.find_all("div")) < 3) : continue    
         [mode, start, end, *remains] = t.find_all("div")
 
         text = t.getText()
-        # print(text)
 
         if re.search(pattern, text):
-            # print(mode.text, start.text, end.text, url)
-            # print(t.text)
             contest_url = 'https://kenkoooo.com/atcoder/'+url
-            # print(contest_url, start)
             notable_contest.add((title, start.text, end.text, contest_url))
 
 try :
     print(notable_contest)
     [title, start, end, contest_url] = notable_contest.pop()
-    
-    tweet(f"test: コンテストのお知らせ -> {title} {contest_url}\
-    ※これはAPIを使った自動ツイートです\
-    #AtCoderProblems")
+
+    # tweet(f"test: コンテストのお知らせ -> {title} {contest_url}\
+    # ※これはAPIを使った自動ツイートです\
+    # #AtCoderProblems")
 except :
     print("error")
-
 
 browser.quit()
