3d4178143e178274a7338758172442c3918074e7
Sakasu02 <s214229y@st.go.tuat.ac.jp>
2023-07-22 01:45:46 +0900

    refactor: imple virtual class using ABC

--- analyzer.py
+++ analyzer.py
@@ -2,18 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-import time 
-from bs4 import BeautifulSoup
-from datetime import datetime
-import re
-from test import tweet
-from enum import Enum
 
-class State(Enum):
-    RECENT = 1
-    UPCOMING_TODAY = 2
-    UPCOMING_FUTURE = 3
-    RUNNING = 4
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+import re
+from enum import Enum
+from contests import State, Contests
 
 today = datetime.today()
 print("Today's date:", today)
@@ -26,20 +20,20 @@ virtual_contests_url = 'https://kenkoooo.com/atcoder/#/contest/recent/'
 browser.get(virtual_contests_url)
 elements = browser.find_elements(By.TAG_NAME, "*")
 
-# pattern = r"[Tt][Uu][Aa][Tt]"
-pattern = r"[A][B][C]"
+pattern = r"(TUAT|ABC|ARC|AGC)"
+pattern = re.compile(r"(TUAT|ABC)", re.IGNORECASE)
 notable_contest = set()
-
-
 
 def getContestState(start, end):
     s = datetime.strptime(start, '%Y-%m-%d %H:%M:%S (%a)')
     e = datetime.strptime(end, '%Y-%m-%d %H:%M:%S (%a)')
     now = datetime.now()
-    print(now, ": [", s, ", ", e, "]")
-
-    # 比較して結果を返す
-    if s > now and s.day == now.day:
+    # print(now, ": [", s, ", ", e, "]")
+    if timedelta(minutes=0) <= s - now <= timedelta(minutes=10):
+        return State.START_ALARM
+    elif timedelta(minutes=0) <= now - e <= timedelta(minutes=10):
+        return State.END_ALARM
+    elif s > now and s.day == now.day:
         return State.UPCOMING_TODAY
     elif s > now :
         return State.UPCOMING_FUTURE
@@ -54,7 +48,7 @@ for element in elements:
     try:
         innerHTML = element.get_attribute("outerHTML")
     except:
-        print("error")
+        print("error")        
 
     if st.__contains__(innerHTML) : continue
     if innerHTML is None: continue
@@ -75,18 +69,13 @@ for element in elements:
         if re.search(pattern, text):
             contest_url = 'https://kenkoooo.com/atcoder/'+url
             notable_contest.add((title, start.text, end.text, contest_url))
-
-runningContests = set()
-upcomingContests = set()  
-recentContests = set()
-
+        
 try :
     for contest in notable_contest:
         [title, start, end, contest_url] = contest
-        # print(start, end, type(start), type(end))
-        print(getContestState(start, end), contest)
-        if getContestState(start, end) == State.UPCOMING_TODAY:
-            upcomingContests.add(contest)
+        res = getContestState(start, end)
+        targetContest = Contests[res]
+        targetContest.add_contest(contest)
 
     while len(notable_contest) > 1:
         [title, start, end, contest_url] = notable_contest.pop()
@@ -94,20 +83,9 @@ try :
 except :
     print("search error")
 
-
-text = ""
-for i in upcomingContests:
-    [title, start, end, contest_url] = i
-    tmp = title + " " + start + " " + contest_url + "\n"
-    if i == 0:
-        text = tmp
-    if len(tmp) > 110 :
-        tweet(f"[本日開催のバーチャルコンテスト]\n{text}#AtCoderProblems")
-        text = ""
-    text += tmp 
-# tweet(f"test: コンテストのお知らせ ->\n {title} {start} {contest_url}\
-#     ※これはAPIを使った自動ツイートです\
-#     #AtCoderProblems")
+for [state, con] in Contests.items():
+    if state in (State.RECENT, State.RUNNING) : continue
+    con.executeTweet()
 
 
 browser.quit()
