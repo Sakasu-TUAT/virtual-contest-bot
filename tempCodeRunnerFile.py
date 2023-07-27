454f87c3dea47427ea6f0488e47dab3122a5f56c
Sakasu02 <s214229y@st.go.tuat.ac.jp>
2023-07-28 01:10:20 +0900

    test: deploy

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-
-# try :
-#     # タプルごとに改行して出力する
-#     for contest in notable_contest:
-#         [title, start, end, contest_url] = contest
-#         print(getContestState(start, end), contest)
-#     while len(notable_contest) > 1:
-#         [title, start, end, contest_url] = notable_contest.pop()
-
-#         notable_contest.pop()
-# except :
-#     print("search error")
+from selenium.webdriver.common.by import By
